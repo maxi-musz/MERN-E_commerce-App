ae6f701981f7d43dddfafe8937af4f191d3559ff
Oragon <omayowagold@gmail.con>
2022-09-26 23:32:27 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,21 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+// Newly Added for deployment
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) => 
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
 app.use(notFound)
 app.use(errorHanlder)
-
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
 
 
 const PORT = process.env.PORT || 5000
